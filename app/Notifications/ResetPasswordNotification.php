7286d51fe27ec5c0b95118a3768ad4a35d7067f3
FawazMrad <FawazMrad10@gmail.com>
2026-05-31 15:10:39 +0300

    [UPDATE] - forgog pass api to send just the code in the email

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -16,16 +16,13 @@ class ResetPasswordNotification extends Notification
 
     public function toMail(object $notifiable): MailMessage
     {
-        $url = env('FRONTEND_URL', 'http://31.70.76.84')
-            . '/reset-password?token=' . $this->token
-            . '&email=' . urlencode($notifiable->email);
-
         return (new MailMessage)
             ->subject('Reset Your Jadal Password')
             ->greeting('Hello, ' . $notifiable->name . '!')
             ->line('You are receiving this email because we received a password reset request for your account.')
-            ->action('Reset Password', $url)
-            ->line('This password reset link will expire in 60 minutes.')
+            ->line('Your password reset code is:')
+            ->line('**' . $this->token . '**')
+            ->line('This code will expire in 60 minutes.')
             ->line('If you did not request a password reset, no further action is required.');
     }
 }
